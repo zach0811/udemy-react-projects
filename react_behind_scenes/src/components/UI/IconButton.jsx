35ac2eac9653043c2a58b2389768a334283cf8f3
Zach Wilkins <zach.d.wilkins@gmail.com>
2024-02-16 19:05:07 -0500

    section 13

---
+++ react_behind_scenes/src/components/UI/IconButton.jsx
@@ -0,0 +1,14 @@
+import { log } from "../../log.js";
+import { memo } from "react";
+
+export const IconButton = memo(({ children, icon, ...props }) => {
+  log("<IconButton /> rendered", 2);
+
+  const Icon = icon;
+  return (
+    <button {...props} className="button">
+      <Icon className="button-icon" />
+      <span className="button-text">{children}</span>
+    </button>
+  );
+});
---
+++ react_behind_scenes/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
+
+// import million from "million/compiler";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [million.vite({ auto: true }), react()],
+// });
